69e368331c6799b7001b4214f5fd02350b0cb633
Albert Fung <albert.fung@live.com>
2019-01-15 11:28:30 -0500

    initial commit

---
+++ background-particles.js
@@ -0,0 +1,83 @@
+var properties ={
+    "particles": {
+      "number": {
+        "value": 40,
+        "density": {
+          "enable": true,
+          "value_area": 800
+        }
+      },
+      "color": {
+        "value": "#000000"
+      },
+      "shape": {
+        "type": "circle",
+        "stroke": {
+          "width": 0,
+          "color": "#000000"
+        },
+        "polygon": {
+          "nb_sides": 5
+        },
+      },
+      "opacity": {
+        "value": 0.7,
+        "random": true,
+        "anim": {
+          "enable": false,
+          "speed": 1,
+          "opacity_min": 0.1,
+          "sync": false
+        }
+      },
+      "size": {
+        "value": 5,
+        "random": true,
+        "anim": {
+          "enable": false,
+          "speed": 80,
+          "size_min": 0.1,
+          "sync": false
+        }
+      },
+      "move": {
+        "enable": true,
+        "speed": 10,
+        "direction": "none",
+        "random": true,
+        "straight": false,
+        "out_mode": "out",
+        "bounce": false,
+        "attract": {
+          "enable": false,
+          "rotateX": 600,
+          "rotateY": 1200
+        }
+      }
+    },
+    "interactivity": {
+      "detect_on": "canvas",
+      "events": {
+        "onhover": {
+          "enable": false,
+          "mode": "repulse"
+        },
+        "onclick": {
+          "enable": false,
+          "mode": "push"
+        },
+        "onresize":{
+          enable:true,
+          density_auto:true,
+          density_area:400
+        },
+        "resize": true
+      },
+    },
+    "retina_detect": true
+  }
+
+  var config = "data:text/plain;base64,"+window.btoa(JSON.stringify(properties));
+  particlesJS.load('particles-js',config, function() {
+    console.log('callback - particles.js config loaded');
+  });
